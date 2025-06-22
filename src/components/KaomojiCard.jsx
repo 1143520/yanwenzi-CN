cfb1fe3292608ff1c3334f089443afb11ca66013
1143520 <2182802547@qq.com>
2025-06-23 00:38:37 +0800

    [BAT] 2025-06-23 00:38

--- src/components/KaomojiCard.jsx
+++ src/components/KaomojiCard.jsx
@@ -22,8 +22,7 @@ function KaomojiCard(props) {
       style={{ backgroundColor: backgroundColor }}
     >
       <div className="card-body">
-        <h5 className="card-title kaomoji-char">{props.kaomoji}</h5>
-        <p className="card-text">{copied ? "已复制！" : null}</p>
+        <h5 className="card-title kaomoji-char">{copied ? "已复制！" : props.kaomoji}</h5>
       </div>
     </div>
   );
